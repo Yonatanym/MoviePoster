aca79ad6ad6aa1fbf39b6e61d4e53d48bbce1c31
Yonatanym <166750160+Yonatanym@users.noreply.github.com>
2024-12-15 12:25:48 -0800

    Update Poster.java

--- app/src/main/java/com/example/poster/Poster.java
+++ app/src/main/java/com/example/poster/Poster.java
@@ -2,9 +2,10 @@ package com.example.poster;
 
 public class Poster {
 
-    String name, createdBy, story;
-    int image;
-    Boolean isSelected= false;
-    float rating;
+    // Declare member variables for the poster details
+    String name, createdBy, story;    // The name of the movie or poster, A brief description, 
+    int image;   // The resource ID of the image (poster) associated with  
+    Boolean isSelected= false;  // Boolean flag to track if the poster is selected or not  
+    float rating;     // The rating of the movie, represented as a float 
 
 }
