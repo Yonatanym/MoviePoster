f27be158fba97387e3f73e8341a2098c01f3c62c
Yonatanym <166750160+Yonatanym@users.noreply.github.com>
2024-12-15 12:36:10 -0800

    Update PostersListener.java

--- app/src/main/java/com/example/poster/PostersListener.java
+++ app/src/main/java/com/example/poster/PostersListener.java
@@ -1,7 +1,8 @@
 package com.example.poster;
 
 public interface PostersListener {
-
+     // Method to notify when a poster is selected or deselected
+    // The boolean 'isSelected' indicates the selection state of the poster
     void onPosterAction(Boolean isSelected);
 
 
